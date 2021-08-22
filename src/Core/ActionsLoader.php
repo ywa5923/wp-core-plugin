33e6287b129ab68372b4104deaf6263ecef68014
Ivan Ion <john007buc@yahoo.com>
2021-08-22 16:51:43 +0300

    fixed bug in ActionsLoader.php

--- src/Core/ActionsLoader.php
+++ src/Core/ActionsLoader.php
@@ -7,24 +7,31 @@ use WOH\File\ScanDirectory;
 class ActionsLoader
 {
 
-    public function loadAllActions($namespace)
+    public function loadAllActions($baseNamespace)
     {
 
-        $actionsDir = plugin_dir_path(dirname(__FILE__, 4)) . 'src/Actions';
+        $actionsDir = plugin_dir_path(dirname(__FILE__, 5)) . 'src/Actions';
 
         $scan = new ScanDirectory($actionsDir);
 
         foreach ($scan->by('php') as $file) {
-            $isAction = strpos($file, 'Action');
 
-            if ($isAction !== 0 || $isAction !== FALSE) {
+            $obj = $this->makeObject($baseNamespace, $file);
 
-                $obj = $this->makeObject($namespace, $file);
+            if ($obj instanceof ActionInterface) {
+                $obj->init();
+            }
+
+            /*$isAction = strpos($file, 'Action');
+
+            if ($isAction !== 0 && $isAction !== FALSE) {
+
+                $obj = $this->makeObject($baseNamespace . "Actions\\", $file);
 
                 if ($obj instanceof ActionInterface) {
                     $obj->init();
                 }
-            }
+            }*/
         }
     }
 
