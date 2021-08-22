ffa1eb9ce61774b03fbd44ba2f10e978b337106a
Ivan Ion <john007buc@yahoo.com>
2021-08-22 16:55:42 +0300

    added exception in Te plateLoader.php

--- src/Core/TemplateLoader.php
+++ src/Core/TemplateLoader.php
@@ -6,19 +6,32 @@ class TemplateLoader
 {
     public static $VIEWS_PATH;
 
-    public static $BLOCKS_PATH;
 
 
     public function __construct(PathFinder $locator)
     {
         self::$VIEWS_PATH = $locator->getViewsPath();
-        self::$BLOCKS_PATH = $locator->getBlocksPath();
     }
 
     public function display($view, array $args)
     {
-        extract($args);
+        $template = $this->resolveTemplateName($view);
+        if (file_exists($template)) {
+            ob_start();
+            extract($args);
+            include $template;
+            return ob_get_clean();
+        } else {
+            throw new \Exception('no template file ' . $template . ' present in directory ' . $this->template_dir);
+        }
 
-        include self::$VIEWS_PATH . $view . '.php';
+
+
+        //include self::$VIEWS_PATH . $view . '.php';
+    }
+
+    public function resolveTemplateName($view)
+    {
+        return self::$VIEWS_PATH . $view . '.php';
     }
 }
